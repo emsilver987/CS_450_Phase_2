61f1014beb3215a69389e107660718732a6e5f67
tolawore1 <olataiwoz113@gmail.com>
2025-11-20 01:36:49 -0500

    Shorten TG name prefix to satisfy AWS limit

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -162,7 +162,7 @@ resource "aws_lb" "validator_lb" {
 }
 
 resource "aws_lb_target_group" "validator_tg" {
-  name_prefix = "validator-"
+  name_prefix = "valtg-"
   port        = 3000
   protocol    = "HTTP"
   vpc_id      = aws_vpc.validator_vpc.id
